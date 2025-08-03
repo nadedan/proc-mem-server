275598e12c74d0c80b1a14a3d875a5d34073bfb7
Daniel Nadeau <Daniel_Nadeau@icloud.com>
2025-08-03 23:19:15 +0000

    emptied main

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"dwarfserver/pkg/procio"
-	"flag"
-)
-
 func main() {
-	elfPath := flag.String("f", "", "elf path")
-	flag.Parse()
-
-	procio.ShowGlobals(*elfPath)
-	//dwarf.ShowStructTypedefs(*elfPath)
-
 }
